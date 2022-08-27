b4fff796edb744405075bf229166143fcb2d7a1e
alanb <efik722life@gmail.com>
2022-08-27 14:25:12 +0900

    fix: add Font Awesome 6 font files

--- Sources/FontAwesomeSwiftUI/FontStyle.swift
+++ Sources/FontAwesomeSwiftUI/FontStyle.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 public enum FontStyle: String, CaseIterable {
-    case brand = "FontAwesome5Brands-Regular"
-    case regular = "FontAwesome5Free-Regular"
-    case solid = "FontAwesome5Free-Solid"
+    case brand = "FontAwesome6Brands-Regular"
+    case regular = "FontAwesome6Free-Regular"
+    case solid = "FontAwesome6Free-Solid"
 
     public var familyName: String {
         switch self {
         case .brand:
-            return "Font Awesome 5 Brands"
+            return "Font Awesome 6 Brands"
         case .regular, .solid:
-            return "Font Awesome 5 Free"
+            return "Font Awesome 6 Free"
         }
     }
 }
